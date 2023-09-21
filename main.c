c30b9df4d1eae3fc51a22b49346e0d3710360346
HyunHee111624 <144989249+HyunHee111624@users.noreply.github.com>
2023-09-21 16:24:15 +0900

    3

--- main.c
+++ main.c
@@ -2,18 +2,14 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  
-  char c;
-  int i;
-  
-  printf("input a number : ");
-  scanf("%c", &c);
-  
-  i = c - '0';
-  printf("The input number ASCII is %i\n", c);
-  printf("The input number is %i\n", i);
-  
-
+    
+    char c;
+    
+    printf("enter a character : ");
+    scanf("%c", &c);
+    
+    printf("The next character of %c (%d) is %c (%d)\n", c, c, c+1, c+1);
+    
   system("PAUSE");	
   return 0;
 }
